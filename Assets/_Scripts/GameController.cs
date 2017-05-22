31cbd2abe93c1fa1bcba799e2893d4a1ab476f2d
Emerson Pinheiro <e_pms@hotmail.com>
2017-05-22 10:50:20 -0300

    commit inicial

---
+++ Assets/_Scripts/GameController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+	public Vector3 spawnValues;
+	public int pickupCount;
+
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
+
+
+	List<GameObject> pickupList = new List<GameObject>();
+	public GameObject Pickup1;
+	public GameObject Pickup2;
+	public GameObject Pickup3;
+
+	void Start()
+	{
+		pickupList.Add(Pickup1);
+		pickupList.Add(Pickup2);
+		pickupList.Add(Pickup3);
+	
+		StartCoroutine (SpawnWaves ());
+	}
+
+	IEnumerator SpawnWaves(){
+
+		yield return new WaitForSeconds (startWait);
+		while (true) {
+			for (int i = 0; i < pickupCount; i++) {
+				Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, 0);
+				Quaternion spawnRotation = Quaternion.identity;
+				int pickupIndex = UnityEngine.Random.Range(0,3);
+				Instantiate (pickupList[pickupIndex], spawnPosition, spawnRotation);
+				yield return new WaitForSeconds (spawnWait);
+			}
+			yield return new WaitForSeconds (waveWait);
+
+		}
+	}
+}
---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody2D rb2d;
+
+
+	void Start()
+	{
+		rb2d = GetComponent <Rigidbody2D> ();
+	}
+
+	void Update()
+	{
+		float moveHorizontal = Input.GetAxisRaw ("Horizontal");
+		Vector3 movement = new Vector3 (moveHorizontal, 0, 0);
+		rb2d.velocity = movement * speed;
+	}
+}
