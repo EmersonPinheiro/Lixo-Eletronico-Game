1ae44698f8f3a83d5eba54c1090a4669336927e3
jnavroski <juniornavroski@hotmail.com>
2017-05-23 23:38:58 -0300

    Add files via upload
    Adicionado menu principal (Iniciar e Sair)

--- Assets/_Scripts/ColectByContact.cs
+++ Assets/_Scripts/ColectByContact.cs
@@ -4,23 +4,9 @@ using UnityEngine;
 
 public class ColectByContact : MonoBehaviour {
 
-	public int scoreValue;
-	private GameController gameController;
-
-	void Start(){
-		GameObject gameControllerObjetc = GameObject.FindGameObjectWithTag ("GameController");
-		if(gameControllerObjetc != null){
-			gameController = gameControllerObjetc.GetComponent<GameController> ();
-		}
-		if(gameController == null){
-			Debug.Log("Cannot find 'GameController' script");
-		}
-	}
-
 	void OnTriggerEnter2D(Collider2D other){
 		if (other.gameObject.tag == "PickUp") {
 			Destroy (other.gameObject);
-			gameController.AddScore (scoreValue);
 		}
 	}
 }
---
+++ Assets/_Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+
+    public void StartGame()
+    {
+
+#pragma warning disable CS0618 // Type or member is obsolete
+        Application.LoadLevel(1);
+#pragma warning restore CS0618 // Type or member is obsolete
+
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
